1a588799ce8fe228299fff22424c9aabc071996d
amany9000 <amanyadavlko2502@gmail.com>
2019-01-13 12:53:31 +0530

    Updated perm2.js

--- src/perm2.js
+++ src/perm2.js
@@ -43,7 +43,7 @@ export function changeValue(steps, actionType) {
 		default:
 			return;
 	}
-
+	console.log("changeValue", actionType)
 	localStorage.setItem('data', JSON.stringify(map));
 
 	return map;
@@ -51,12 +51,11 @@ export function changeValue(steps, actionType) {
 
 export function move(currentPerm, turn) {
 	let nextState = '';
-	let i,
-		tot = 0;
+	let i,tot = 0,j;
 	let rand = Math.random();
 	let probCount = 0;
-
-	for (i in map[currentPerm].val) {
+	//console.log("map",map, currentPerm)
+	for (i in map[currentPerm].val){
 		if (map[currentPerm].val[i].turn === turn) {
 			tot += map[currentPerm].val[i].value;
 		}
@@ -70,6 +69,18 @@ export function move(currentPerm, turn) {
 			break;
 		}
 	}
+	let value = [];
+	let count = 0;
+	for (j = 0; j < nextState; j++) {
+		if (nextState[j] === '0') {
+			let valString = nextState.slice(0, j) + '2' + nextState.slice(j + 1, nextState.length);
+			let valString2 = nextState.slice(0, j) + '1' + nextState.slice(j + 1, nextState.length);
+			value.push({ perm: valString, value: 4, turn: '2' });
+			value.push({ perm: valString2, value: 4, turn: '1' });
+			count++;
+		}
+	}
+	map[nextState] = {"val":value, "count": count}
 	return checkFinish(nextState);
 }
 
@@ -84,8 +95,10 @@ export function checkFinish(currentPerm) {
 		(currentPerm[0] === '1' && currentPerm[4] === '1' && currentPerm[8] === '1') ||
 		(currentPerm[2] === '1' && currentPerm[4] === '1' && currentPerm[6] === '1')
 	) {
+		console.log("Win")
 		return {
-			reply: 'Win'
+			reply: 'Win',
+			currentPerm
 		};
 	} else if (
 		(currentPerm[0] === '2' && currentPerm[1] === '2' && currentPerm[2] === '2') ||
@@ -97,6 +110,7 @@ export function checkFinish(currentPerm) {
 		(currentPerm[0] === '2' && currentPerm[4] === '2' && currentPerm[8] === '2') ||
 		(currentPerm[2] === '2' && currentPerm[4] === '2' && currentPerm[6] === '2')
 	) {
+		console.log("lose")
 		return {
 			reply: 'Lose',
 			currentPerm
@@ -111,10 +125,13 @@ export function checkFinish(currentPerm) {
 	}
 
 	if (zeroCount === 0) {
+		console.log("Draw")
 		return {
-			reply: 'Draw'
+			reply: 'Draw',
+			currentPerm
 		};
 	} else {
+		console.log("Continue")
 		return {
 			reply: 'Continue',
 			currentPerm
@@ -125,55 +142,27 @@ export function checkFinish(currentPerm) {
 if (localStorage.getItem('data')) {
 	map = Object.assign({}, JSON.parse(localStorage.getItem('data')));
 } else {
-	// The method that prints all
-	// possible strings of length k.
-	// It is mainly a wrapper over
-	// recursive function printAllKLengthRec()
-	function printAllKLength(set, k) {
-		let n = set.length;
-		printAllKLengthRec(set, '', n, k);
-	}
-
-	// The main recursive method
-	// to print all possible
-	// strings of length k
-	function printAllKLengthRec(set, prefix, n, k) {
-		// Base case: k is 0,
-		// print prefix
-		if (k === 0) {
-			perm.push(prefix);
-			return;
-		}
-
-		// One by one add all characters
-		// from set and recursively
-		// call for k equals to k-1
-		for (let i = 0; i < n; ++i) {
-			// Next character of input added
-			let newPrefix = `${prefix}${set[i]}`;
-			// k is decreased, because
-			// we have added a new character
-			printAllKLengthRec(set, newPrefix, n, k - 1);
-		}
-	}
-
-	let perm = [];
-	let set = ['1', '2', '0'];
-	printAllKLength(set, 9);
-
-	for (let i in perm) {
-		let count = 0;
-		let val = [];
-		for (let j = 0; j < perm[i].length; j++) {
-			if (perm[i][j] === '0') {
-				let valString = perm[i].slice(0, j) + '2' + perm[i].slice(j + 1, perm[i].length);
-				let valString2 = perm[i].slice(0, j) + '1' + perm[i].slice(j + 1, perm[i].length);
-				val.push({ perm: valString, value: 4, turn: '2' });
-				val.push({ perm: valString2, value: 4, turn: '1' });
-				count++;
-			}
-		}
-
-		map[perm[i]] = { count: count, val: val };
-	}
+	let value = [];
+	
+	value.push({"perm": "200000000","value": 4, "turn": "2"})
+	value.push({"perm": "100000000","value": 4, "turn": "1"})
+	value.push({"perm": "020000000","value": 4, "turn": "2"})
+	value.push({"perm": "010000000","value": 4, "turn": "1"})
+	value.push({"perm": "002000000","value": 4, "turn": "2"})
+	value.push({"perm": "001000000","value": 4, "turn": "1"})
+	value.push({"perm": "000200000","value": 4, "turn": "2"})
+	value.push({"perm": "000100000","value": 4, "turn": "1"})
+	value.push({"perm": "000020000","value": 4, "turn": "2"})
+	value.push({"perm": "000010000","value": 4, "turn": "1"})
+	value.push({"perm": "000002000","value": 4, "turn": "2"})
+	value.push({"perm": "000001000","value": 4, "turn": "1"})
+	value.push({"perm": "000000200","value": 4, "turn": "2"})
+	value.push({"perm": "000000100","value": 4, "turn": "1"})
+	value.push({"perm": "000000020","value": 4, "turn": "2"})
+	value.push({"perm": "000000010","value": 4, "turn": "1"})
+	value.push({"perm": "000000002","value": 4, "turn": "2"})
+	value.push({"perm": "000000001","value": 4, "turn": "1"})
+	map["000000000"] = {"val": value, "count": 18}
+	//console.log("init", map)
 }
+
